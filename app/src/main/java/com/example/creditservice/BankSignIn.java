b9ee7659cc96d2ee6112cdcb0e3cfd3ff0c0791b
molitvin2813 <molitvin17@mail.ru>
2020-03-03 22:39:21 +0300

    реализована возможность входа пользователей #2, #3. Добавлена БД

--- app/src/main/java/com/example/creditservice/BankSignIn.java
+++ app/src/main/java/com/example/creditservice/BankSignIn.java
@@ -2,13 +2,59 @@ package com.example.creditservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+
 
 public class BankSignIn extends AppCompatActivity {
+    private DataBaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
+    EditText loginBank;
+    EditText passwordBank;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_sign_in);
+
+        loginBank = (EditText) findViewById(R.id.editTextLoginBank);
+        passwordBank = (EditText) findViewById(R.id.editTextPasswordBank);
     }
+
+    /**
+     * Метод для входа в систему представителя банка
+     * @param view авлпт
+     */
+    public void btnBankSignInClick(View view){
+        mDBHelper = new DataBaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+
+        Cursor cursor = mDb.rawQuery("SELECT * FROM t_bank WHERE t_bank.login='"+loginBank.getText().toString() + "' ", null);
+        cursor.moveToFirst();
+
+        if (!cursor.isAfterLast())
+            if(cursor.getString(2).equals(passwordBank.getText().toString()))
+                loginBank.setText("Вход выполнен");
+
+        cursor.close();
+        mDBHelper.close();
+    }
+
 }
--- app/src/main/java/com/example/creditservice/UserSignIn.java
+++ app/src/main/java/com/example/creditservice/UserSignIn.java
@@ -2,13 +2,59 @@ package com.example.creditservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
 
 public class UserSignIn extends AppCompatActivity {
+
+    private DataBaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
+
+    EditText loginUser;
+    EditText passwordUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_in);
+        loginUser = (EditText) findViewById(R.id.editTextLoginUser);
+        passwordUser = (EditText) findViewById(R.id.editTextPasswordUser);
+
+    }
+
+    /**
+     * Метод для входа обычного пользователя системы
+     * @param view выа
+     */
+    public void btnUserSignInClick(View view){
+        mDBHelper = new DataBaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+
+        Cursor cursor = mDb.rawQuery("SELECT * FROM t_user WHERE t_user.login = '"+loginUser.getText().toString() + "' ", null);
+        cursor.moveToFirst();
+
+        if (!cursor.isAfterLast())
+            if(cursor.getString(2).equals(passwordUser.getText().toString()))
+                loginUser.setText("Вход выполнен");
+
+        cursor.close();
+        mDBHelper.close();
     }
 }
